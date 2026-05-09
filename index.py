64992c8cfca5d3d114b4c0279c735fb426624f33
AnB School 0007 <anbschool0007@AnBs-Mac-mini-32.local>
2026-05-09 15:46:15 +0700

    Done

--- index.py
+++ index.py
@@ -3,5 +3,5 @@ def my_function(*args):
   print("First argument:", args[0])
   print("Second argument:", args[1])
   print("All arguments:", args)
-
+ ##### Calling the function with three arguments
 my_function("Emil", "Tobias", "Linus")
